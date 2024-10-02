6ebf5a898e7c4b3b1931254d59843a88dbadddfd
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-10-02 16:23:50 -0300

    Taxa real

---
+++ RealTax.java
@@ -0,0 +1,32 @@
+public class RealTax {
+    double taxaAparente;
+    double inflacao;
+    double taxaReal;
+
+    public RealTax(double taxaAparente, double inflacao) {
+        this.taxaAparente = taxaAparente;
+        this.inflacao = inflacao;
+        this.calcular();
+    }
+
+    public void calcular() {
+        this.taxaReal = ((1 + this.taxaAparente / 100) / (1 + this.inflacao / 100) - 1) * 100;
+    }
+
+    public double getTaxaReal() {
+        return this.taxaReal;
+    }
+
+    public void setTaxaAparente(double taxaAparente) {
+        this.taxaAparente = taxaAparente;
+    }
+
+    public void setInflacao(double inflacao) {
+        this.inflacao = inflacao;
+    }
+
+    public static void main(String[] args) {
+        RealTax taxa = new RealTax(6, 5);
+        System.out.println("\n Taxa real: " + taxa.getTaxaReal() + " %\n");
+    }
+}
